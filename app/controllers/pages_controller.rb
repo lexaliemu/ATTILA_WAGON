701cd396d0d5ab2ccd98b11bdf51ff3e4fb0fcef
Alice Bouffard <alice.bouffard@gmail.com>
2018-12-07 15:39:07 +0100

    Alice modifs HP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,25 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # THRILLER MOVIES
+    @thriller_categories = Category.where("name ILIKE ?", "%thriller%")
+    @thriller_movies = Movie.joins(:movie_categories).
+      where(movie_categories: { category_id: @thriller_categories.pluck(:id) }).
+      limit(4)
+    # ROMANCE MOVIES
+    @romance_categories = Category.where("name ILIKE ?", "%romance%")
+    @romance_movies = Movie.joins(:movie_categories).
+      where(movie_categories: { category_id: @romance_categories.pluck(:id) }).
+      limit(4)
+    # DRAMA MOVIES
+    @drama_categories = Category.where("name ILIKE ?", "%drama%")
+    @drama_movies = Movie.joins(:movie_categories).
+      where(movie_categories: { category_id: @drama_categories.pluck(:id) }).
+      limit(4)
+    # ADVENTURE MOVIES
+    @adventure_categories = Category.where("name ILIKE ?", "%adventure%")
+    @adventure_movies = Movie.joins(:movie_categories).
+      where(movie_categories: { category_id: @adventure_categories.pluck(:id) }).
+      limit(4)
   end
 end
