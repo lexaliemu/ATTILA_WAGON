65ff896a8adc3e4a062dc302529383e1b0294777
william segard <william.segard@gmail.com>
2018-12-10 13:15:16 +0100

    adding follower/followee model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
     @playlists = @user.created_playlists
@@ -12,5 +13,39 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+
+  def index
+    @users = User.all.where.not(id: current_user.id)
+  end
+
+  before_action :set_user, only: [:follow, :unfollow]
+
+  def index
+    @users = User.all.where.not(id: current_user.id)
+  end
+
+  def follow
+    if current_user.follow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
+  end
+
+  def unfollow
+    if current_user.unfollow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { render action: :follow }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
