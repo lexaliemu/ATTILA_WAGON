3a41bea8b82905e21317228846c6171a0292e692
Alice Bouffard <alice.bouffard@gmail.com>
2018-12-11 12:54:27 +0100

    checklist done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :username, :first_name, :last_name) }
     # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit( :first_name, :last_name, :age, :country, :biography, :email, :password, :current_password, :photo, :photo_cache, :remove_avatar) }
   end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
+
+
