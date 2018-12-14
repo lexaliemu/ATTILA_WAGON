04981f06dfa9e940ad4567b52e77a89a201b0135
william segard <william.segard@gmail.com>
2018-12-14 11:03:06 +0100

    ws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.url = "wss://www.myattila.top/cable"
+  config.action_cable.url = "ws://www.myattila.top/cable"
   config.action_cable.allowed_request_origins = [ "http://www.myattila.top", "https://www.myattila.top" ]
 
 end
